6503940ff8878828a2a3c533b6a0cb6cfdc29e84
Rascal_Two <therealrascaltwo@gmail.com>
2022-04-23 16:12:36 -0400

    Add game over screen
    Add screen exclusivity

--- js/index.js
+++ js/index.js
@@ -1,10 +1,47 @@
-const QUESTION_TITLE = document.querySelector('h1');
-const ANSWERS_CONTAINER = document.querySelector('#answers-container');
+const MAIN = document.querySelector('main');
+const GAME_OVER = document.querySelector('#game-over');
+const QUESTION_TITLE = document.querySelector('#game-play h1');
+const ANSWERS_CONTAINER = document.querySelector('#game-play .answers-container');
 
-const answerIndexes = []
+function shuffle(array) {
+  let currentIndex = array.length,  randomIndex;
+
+  while (currentIndex != 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
+}
+
+
+function showScreen(showing) {
+	for (const child of MAIN.children) {
+		child.classList.toggle('hidden', child.id !== showing)
+	}
+}
+
+document.querySelector('#start-game button').addEventListener('click', () => {
+	fetchQuestions().then((data) => {
+		questions.push(...data.results.map(raw => {
+			const correctAnswer = raw.correct_answer.trim()
+			return {
+				question: raw.question,
+				answers: shuffle([correctAnswer, ...raw.incorrect_answers.map(incorrect => incorrect.trim())]),
+				correctAnswer
+			}
+		}));
+		showScreen('game-play');
+		renderQuestion();
+	});
+})
+
+const answerIndexes = [];
 document.querySelector('form').addEventListener('change', event => {
-	const answerIndex = +event.target.value
-	answerIndexes.push(answerIndex)
+	const answerIndex = +event.target.value;
+	answerIndexes.push(answerIndex);
 
 	const question = questions[currentQuestionIndex];
 	if (question.answers[answerIndex] === question.correctAnswer) {
@@ -14,13 +51,40 @@ document.querySelector('form').addEventListener('change', event => {
 	}
 
 	currentQuestionIndex++;
-	if (currentQuestionIndex >= questions.length){
-		alert('Out of question');
+	if (currentQuestionIndex >= questions.length) {
+		showScreen('game-over');
+		renderGameOver();
 	} else {
-		renderQuestion()
+		renderQuestion();
 	}
 })
 
+function renderGameOver() {
+	const ul = GAME_OVER.querySelector('ul');
+	ul.innerHTML = '';
+
+	for (let q = 0; q < questions.length; q++) {
+		const question = questions[q];
+		let html = `
+			<li>
+				<fieldset>
+					<legend>${question.question}</legend>
+					<div class="answers-container">`
+		for (let a = 0; a < question.answers.length; a++) {
+			const checked = answerIndexes[q] === a ? 'checked' : ''
+			html += `
+				<input id="q-${q}-answer-${a}" type="radio" value="${a}" name="q-${q}-answer" ${checked} disabled />
+				<label for="q-${q}-answer-${a}" ${question.answers[a] === question.correctAnswer ? 'class="correct-answer"' : ''}>${question.answers[a]}</label>
+			`
+		}
+		html += `
+				</div>
+			</fieldset>
+		</li>
+		`
+		ul.innerHTML += html
+	}
+}
 
 const questions = [];
 let currentQuestionIndex = 0;
@@ -36,18 +100,6 @@ function renderQuestion() {
 		`
 	}
 }
-
-fetchQuestions().then((data) => {
-	questions.push(...data.results.map(raw => {
-		const correctAnswer = raw.correct_answer.trim()
-		return {
-			question: raw.question,
-			answers: [correctAnswer, ...raw.incorrect_answers.map(incorrect => incorrect.trim())],
-			correctAnswer
-		}
-	}));
-	renderQuestion();
-});
 
 function fetchQuestions() {
 	return fetch('https://opentdb.com/api.php?amount=3')
